6c23bad53d86f81111d33214d5c80107f8b8ec64
Near <88817735+NearVPN@users.noreply.github.com>
2022-10-02 02:30:42 -0400

    Update tcpspeed.sh

--- online/tcpspeed.sh
+++ online/tcpspeed.sh
@@ -1,15 +1,24 @@
-#!/bin/bash
-
-TCPspeed () {
+function_5 () {
+msg -bar 
+echo -e "${cor[4]} Este Script fue proyectado"
+echo -e "${cor[4]} Para Mejorar La Latencia"
+echo -e "${cor[4]} y velocidad del servidor!"
+msg -bar 
+echo -e "${cor[5]} analizar"
+sleep 1s
 if [[ `grep -c "^#ADM" /etc/sysctl.conf` -eq 0 ]]; then
 #INSTALA
-echo -e "$(msg -azu "TCP Speed No Activado, Desea Activar Ahora")?"
-msg -bar
-while [[ ${resposta} != @(s|S|n|N|y|Y) ]]; do
-read -p " [S/N]: " -e -i s resposta
-tput cuu1 && tput dl1
-done
-[[ "$resposta" = @(s|S|y|Y) ]] && {
+echo -e "${cor[5]} Este es un script experimental"
+echo -e "${cor[5]} ¡Utilice por su propia cuenta y riesgo!"
+echo -e "${cor[5]} Este script cambiará algunas"
+echo -e "${cor[5]} configuraciones de red (BBR)"
+echo -e "${cor[5]} del sistema para reducir"
+echo -e "${cor[5]} la latencia y mejorar la velocidad"
+msg -bar #echo -e "${cor[1]} ●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●● ${cor[0]}"
+read -p " Continuar con la instalación? [s/n]: " -e -i s resp_osta
+echo -e "\033[1;37m"
+if [[ "$resp_osta" = 's' ]]; then
+unset resp_osta
 echo "#ADM" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_window_scaling = 1
 net.core.rmem_max = 16777216
@@ -17,19 +26,26 @@ net.core.wmem_max = 16777216
 net.ipv4.tcp_rmem = 4096 87380 16777216
 net.ipv4.tcp_wmem = 4096 16384 16777216
 net.ipv4.tcp_low_latency = 1
-net.ipv4.tcp_slow_start_after_idle = 0" >> /etc/sysctl.conf
+net.ipv4.tcp_slow_start_after_idle = 0
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p /etc/sysctl.conf > /dev/null 2>&1
-echo -e "$(msg -azu "TCP Activo Con Exito")!"
-} || echo -e "$(msg -azu "Cancelado")!"
+echo -e "${cor[5]} Configuración de red TCP"
+echo -e "${cor[5]} se han agregado con éxito"
+msg -bar #echo -e "${cor[1]} ●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●● ${cor[0]}"
+return
+ else
+msg -bar 
+return
+fi
  else
 #REMOVE
-echo -e "$(msg -azu "TCP Speed ya esta activado, desea detener ahora")?"
+echo -e "${cor[5]} Configuración de red TCP"
+echo -e "${cor[5]} ya se han agregado en el sistema!"
 msg -bar
-while [[ ${resposta} != @(s|S|n|N|y|Y) ]]; do
-read -p " [S/N]: " -e -i s resposta
-tput cuu1 && tput dl1
-done
-[[ "$resposta" = @(s|S|y|Y) ]] && {
+read -p " Desea quitar la configuración TCP? [s/n]: " -e -i n res_posta
+if [[ "$res_posta" = 's' ]]; then
+unset res_posta
 grep -v "^#ADM
 net.ipv4.tcp_window_scaling = 1
 net.core.rmem_max = 16777216
@@ -37,33 +53,17 @@ net.core.wmem_max = 16777216
 net.ipv4.tcp_rmem = 4096 87380 16777216
 net.ipv4.tcp_wmem = 4096 16384 16777216
 net.ipv4.tcp_low_latency = 1
-net.ipv4.tcp_slow_start_after_idle = 0" /etc/sysctl.conf > /tmp/syscl && mv -f /tmp/syscl /etc/sysctl.conf
+net.ipv4.tcp_slow_start_after_idle = 0
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr" /etc/sysctl.conf > /tmp/syscl && mv -f /tmp/syscl /etc/sysctl.conf
 sysctl -p /etc/sysctl.conf > /dev/null 2>&1
-echo -e "$(msg -azu "TCP Parado Con Exito")!"
-} || echo -e "$(msg -azu "Cancelado")!"
+echo -e "${cor[5]} Configuración de red TCP"
+echo -e "${cor[5]} se han eliminado con éxito"
+msg -bar
+return
+ else
+msg -bar 
+return
+ fi
 fi
 }
-menuTCPspeed(){
-    [[ $(crontab -l|grep '@reboot'|grep 'service'|grep 'stunnel4') ]] && actfix='\e[1m\e[32m[ON]' || actfix='\e[1m\e[31m[OFF]'
-	on="\033[1;32m[ON]" && off="\033[1;31m[OFF]"
-    [[ `grep -c "^#ADM" /etc/sysctl.conf` -eq 0 ]] && tcp=$off || tcp=$on
-    title "INSTALADOR TCPspeed By @Near365"
-    echo -e "$(msg -verd " [1]") $(msg -verm2 ">") $(msg -verd "INSTALAR") $(msg -ama "-") $(msg -verm2 "DESINSTALAR")"
-    n=1
-    if [[ `grep -c "^#ADM" /etc/sysctl.conf` -eq 0 ]]; then
-        msg -bar3
-        echo -e "$(msg -verd " [7]") $(msg -verm2 ">") $(msg -azu "INICIAR/DETENER TCPspeed")"
-        n=2
-    fi
-    back
-    opcion=$(selection_fun $n)
-    case $opcion in
-        1)TCPspeed;;
-        2)TCPspeed;;
-        0)return 1;;
-    esac
-}
-
-while [[  $? -eq 0 ]]; do
-  menuTCPspeed
-done
